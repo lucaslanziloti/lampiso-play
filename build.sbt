8830fff66e8a70b3104d3b60646ee9a17493bbb7
lucaslanziloti <lucas.lanziloti@vitaebrasil.com.br>
2017-08-29 15:14:40 -0300

    FIrst commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "lampiso-play"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/home/lucas/PlayProjects/lampiso-play/conf/routes
+// @HASH:9c24f09449355737d0bfbe9ca53280febbd777c7
+// @DATE:Tue Aug 29 15:13:33 BRT 2017
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+}
+          
